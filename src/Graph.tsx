ca1b17e1d4ee84dcad82d1d4f8db37ee3fb7d2b6
Lucas Stevanin <lucasstevanin@gmail.com>
2023-04-18 15:38:27 -0300

    task two

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -36,21 +36,6 @@ class Graph extends Component<IProps, {}> {
       "perspective-viewer"
     )[0] as unknown) as PerspectiveViewerElement;
 
-    elem.setAttribute("view", "y_line");
-    elem.setAttribute("column-pivots", '["stock"]');
-    elem.setAttribute("row-pivots", '["timestamp"]');
-    elem.setAttribute("columns", '["top_ask_price"]');
-    elem.setAttribute(
-      "aggregates",
-      `
-      {
-      "stock" : "distinct count",
-      "top_ask_price" : "avg"
-      "top_bid_price" : "avg"
-      "timestamp" : "distinct count"
-      }`
-    );
-
     const schema = {
       stock: "string",
       top_ask_price: "float",
@@ -66,6 +51,20 @@ class Graph extends Component<IProps, {}> {
 
       // Add more Perspective configurations here.
       elem.load(this.table);
+      elem.setAttribute("view", "y_line");
+      elem.setAttribute("column-pivots", '["stock"]');
+      elem.setAttribute("row-pivots", '["timestamp"]');
+      elem.setAttribute("columns", '["top_ask_price"]');
+      elem.setAttribute(
+        "aggregates",
+        `
+        {
+        "stock" : "distinct count",
+        "top_ask_price" : "avg"
+        "top_bid_price" : "avg"
+        "timestamp" : "distinct count"
+        }`
+      );
     }
   }
 
